b05324f1644611dfc077a584588217f526d0b03b
William Mabotja <130538991+williammabotjaioco@users.noreply.github.com>
2023-05-20 19:18:01 +0200

    Update PortalContext.tsx

--- src/PortalContext.tsx
+++ src/PortalContext.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 
 export const portalContext = React.createContext({
-    name: "",
-    surname: "",
-    country: ""
+    key: "",
+    children: React.ReactNode
 });
