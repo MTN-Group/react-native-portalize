42ec29af11d2ae8e78f88585af3d1f194b4e584f
William Mabotja <130538991+williammabotjaioco@users.noreply.github.com>
2023-05-25 10:46:30 +0200

    Update Host.tsx

--- src/Host.tsx
+++ src/Host.tsx
@@ -15,7 +15,7 @@ export interface IProvider {
   unmount(key?: string): void;
 }
 
-export let globalKey = "";
+export let globalKey = null;
 
 export const Context = React.createContext<IProvider | null>(null);
 
